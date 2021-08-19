61939d79bf9880134f9179e56e554f750f093d33
Bbanez <banezprod@gmail.com>
2021-08-19 16:58:00 +0200

    Improve usage

--- bcms.config.js
+++ bcms.config.js
@@ -1,16 +1,21 @@
+require('module-alias/register');
+const { createBcmsConfig } = require('@becomes/cms-backend');
+
 const pluginName = 'hello---world';
 process.env.VUE_APP_PLUGIN_NAME = pluginName;
 
-module.exports = {
-  port: parseInt(process.env.PORT, 10),
+module.exports = createBcmsConfig({
+  port: process.env.PORT ? parseInt(process.env.PORT, 10) : 1280,
   jwt: {
-    expireIn: parseInt(process.env.JWT_EXP_AFTER, 10),
-    scope: process.env.JWT_SCOPE,
-    secret: process.env.JWT_SECRET,
+    expireIn: process.env.JWT_EXP_AFTER
+      ? parseInt(process.env.JWT_EXP_AFTER, 10)
+      : 60000,
+    scope: process.env.JWT_SCOPE || 'localhost',
+    secret: process.env.JWT_SECRET || 'secret',
   },
   database: {
-    prefix: process.env.DB_PRFX,
+    prefix: process.env.DB_PRFX || 'bcms',
     fs: true,
   },
   plugins: [pluginName],
-};
+});
