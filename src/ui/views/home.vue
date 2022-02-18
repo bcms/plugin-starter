111441342ded3e67822827f99ad6443f42fb4e52
Bbanez <banezprod@gmail.com>
2022-02-18 13:02:40 +0100

    Fix store access

--- src/ui/views/home.vue
+++ src/ui/views/home.vue
@@ -1,16 +1,14 @@
 <script lang="tsx">
-import { BCMSTemplate, BCMSStoreMutationTypes } from '@becomes/cms-sdk/types';
-import { BCMSButton } from '@becomes/cms-ui/components';
-import { computed, defineComponent, onUnmounted, ref } from '@vue/runtime-core';
-import { useStore } from '../store';
-import { StoreMutationTypes } from '../types';
+import { BCMSTemplate } from '@becomes/cms-sdk/types';
+import { BCMSStoreMutationTypes } from '@becomes/cms-ui/types';
+import { defineComponent, onUnmounted, ref } from '@vue/runtime-core';
 
 const component = defineComponent({
   setup() {
     const templates = ref<BCMSTemplate[]>(
-      Object.assign([], window.bcms.sdk.store.getters.template_items)
+      Object.assign([], window.bcms.vue.store.getters.template_items)
     );
-    const bcmsStoreUnsub = window.bcms.sdk.store.subscribe(
+    const bcmsStoreUnsub = window.bcms.vue.store.subscribe(
       (mutation, state) => {
         if (mutation.type === BCMSStoreMutationTypes.template_set) {
           templates.value = Object.assign({}, state.template);
